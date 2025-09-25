cbee073e79a5e5569978d9dcfa81a89f7f14a687
salitsamyl <salitsameylani@gmail.com>
2025-09-25 21:53:04 +0700

    Update

--- lib/tugas/layout3.dart
+++ lib/tugas/layout3.dart
@@ -33,7 +33,8 @@ class Layout3 extends StatelessWidget {
             height: 200,
             width: double.infinity,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)), 
+              borderRadius: BorderRadius.only(topRight: Radius.circular(120), topLeft: Radius.circular(20),
+              bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
               gradient: LinearGradient(begin: AlignmentGeometry.centerLeft, end: AlignmentGeometry.centerRight,
               colors: [const Color.fromARGB(255, 170, 52, 162), const Color.fromARGB(255, 213, 140, 245)],
             ),
@@ -46,9 +47,19 @@ class Layout3 extends StatelessWidget {
                   Text("Next Workout", style: TextStyle(color: Colors.white),),
                   Text("Lets Toning", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),),
                   Text("and Glutes Workout", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),),
+                  SizedBox(height: 60,),
+                  Text("60 min", style: TextStyle(color: Colors.white),)
                 ],
               ),
             ),
+          ),
+          SizedBox(height: 20,),
+          Stack(
+            children: [
+              Container(
+                child: Positioned(child: Image.asset("gambar/card.jpg", width: 600 ,) ),
+              )
+            ],
           )
           ],
         ),
