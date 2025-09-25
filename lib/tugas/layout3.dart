dcaa66364932a0036e60c405ca79d422ba42fce1
salitsamyl <salitsameylani@gmail.com>
2025-09-25 21:10:04 +0700

    update

--- lib/tugas/layout3.dart
+++ lib/tugas/layout3.dart
@@ -20,16 +20,27 @@ class Layout3 extends StatelessWidget {
                 Icon(Icons.chevron_right)
               ],
             ),
+            SizedBox(height: 20,),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                child: Text("Your Program", style: TextStyle(fontWeight:FontWeight.bold,fontSize: 18 ),
-              ),
-              ),
+                Text("Your Program", style: TextStyle(fontWeight:FontWeight.bold,fontSize: 17 ),),
+                Text("Details", style: TextStyle(fontWeight:FontWeight.bold,fontSize: 15, color: Colors.blue),),
+                Icon(Icons.chevron_right, color: Colors.blue  ,)
             ],
           ),
+          SizedBox(height: 20,),
+          Container(
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.horizontal((20)), 
+              gradient: LinearGradient(begin: AlignmentGeometry.centerLeft, end: AlignmentGeometry.centerRight,
+              colors: [const Color.fromARGB(255, 186, 78, 179), const Color.fromARGB(255, 203, 118, 239)],
+            ),
+            ),
+            child: Column(),
+          )
           ],
         ),
       ),
