10237c3ec64fef29fb9c54ca13918941e3e3b165
salitsamyl <salitsameylani@gmail.com>
2025-09-25 21:24:54 +0700

    Update

--- lib/tugas/layout3.dart
+++ lib/tugas/layout3.dart
@@ -15,9 +15,7 @@ class Layout3 extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [Container(
                 child: Text("Training", style: TextStyle(fontWeight:FontWeight.bold,fontSize: 23 ),)),
-                Icon(Icons.chevron_left),
-                Icon(Icons.calendar_month),
-                Icon(Icons.chevron_right)
+            
               ],
             ),
             SizedBox(height: 20,),
@@ -27,19 +25,30 @@ class Layout3 extends StatelessWidget {
             children: [
                 Text("Your Program", style: TextStyle(fontWeight:FontWeight.bold,fontSize: 17 ),),
                 Text("Details", style: TextStyle(fontWeight:FontWeight.bold,fontSize: 15, color: Colors.blue),),
-                Icon(Icons.chevron_right, color: Colors.blue  ,)
+
             ],
           ),
           SizedBox(height: 20,),
           Container(
             height: 200,
+            width: double.infinity,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.horizontal((20)), 
+              borderRadius: BorderRadius.all(Radius.circular(20)), 
               gradient: LinearGradient(begin: AlignmentGeometry.centerLeft, end: AlignmentGeometry.centerRight,
-              colors: [const Color.fromARGB(255, 186, 78, 179), const Color.fromARGB(255, 203, 118, 239)],
+              colors: [const Color.fromARGB(255, 170, 52, 162), const Color.fromARGB(255, 213, 140, 245)],
             ),
             ),
-            child: Column(),
+            child: Container(
+              padding: EdgeInsets.only(left: 20,top: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Next Workout", style: TextStyle(color: Colors.white),),
+                  Text("Lets Toning", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),),
+                  Text("and Glutes Workout", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),),
+                ],
+              ),
+            ),
           )
           ],
         ),
